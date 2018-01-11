29e02946d0e9256bb348b73be160f1a883766ec5
Churjan <churjan@163.com>
2018-01-11 17:56:46 +0800

    add random color func

--- src/random.js
+++ src/random.js
@@ -1,10 +1,20 @@
 /**
- * 
+ *
  * @desc 生成指定范围[min, max]的随机数
- * @param  {Number} min 
- * @param  {Number} max 
- * @returns {Number} 
+ * @param  {Number} min
+ * @param  {Number} max
+ * @returns {Number}
  */
 function randomNum(min, max) {
-    return Math.floor(Math.random() * (max-min+1) )+ min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/**
+ * 
+ * @desc 生成随机颜色
+ * @returns {String}
+ */
+function randomColor() {
+  return (
+    "#" + ("00000" + ((Math.random() * 0x1000000) << 0).toString(16)).slice(-6)
+  );
 }
