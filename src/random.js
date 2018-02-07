7ead8875af4c862ac372b3e7f83c741512234649
Churjan <churjan@163.com>
2018-02-07 11:24:37 +0800

    edit

--- src/random.js
+++ src/random.js
@@ -1,5 +1,4 @@
 /**
- *
  * @desc 生成指定范围[min, max]的随机数
  * @param  {Number} min
  * @param  {Number} max
@@ -9,7 +8,6 @@ function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 /**
- * 
  * @desc 生成随机颜色
  * @returns {String}
  */
