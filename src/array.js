bbd6e772e7a13f24c69726c09673fc2ece70fa35
Churjan <churjan@163.com>
2018-02-07 10:21:50 +0800

    edit

--- src/array.js
+++ src/array.js
@@ -1,9 +1,8 @@
 /**
- * 
  * @desc 将一维数组变成二维数组
- * @param {A} arr 
+ * @param {Array} arr 
  * @param {number} [len=2] 每组数组的长度
- * @returns 返回一个二维数组
+ * @returns {Array} 返回一个二维数组
  */
 function oneArrToTwoArr(arr, len = 2) {
   let oneArr = arr;
@@ -18,12 +17,24 @@ function oneArrToTwoArr(arr, len = 2) {
   });
   return twoArr;
 }
+
 /**
- * 
  * @desc 数组去重
  * @param {Array} arr 
  * @returns 
  */
 function removeRepeatArray(arr) {
-  return Array.from(new Set(arr))
+  return Array.from(new Set(arr));
+  // return [...new Set(arr)]; //第二种写法
+}
+
+/**
+ * @desc 计数数组中值的出现次数
+ * @param {Array} arr
+ * @param {number} value
+ * @returns {number} 返回值出现的次数
+ * @example countOccurrences([1,1,1,2,3,4,5],1) // 3
+ */
+function countOccurrences (arr,value) {
+  return arr.reduce((sum,v)=>v===value?sum+1:sum+0,0);
 }
