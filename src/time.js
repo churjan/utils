0933489ac75c2edf98e4502ffea46947bcc66c25
churjan <churjan@163.com>
2018-07-27 15:15:30 +0800

    'add'

--- src/time.js
+++ src/time.js
@@ -4,7 +4,12 @@
  * @returns {string}
  */
 function timestampToTime(timestamp) {
-  var date = new Date(timestamp); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  if(timestamp.length==10){
+    let date = new Date(timestamp*1000); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  }else{
+    var date = new Date(timestamp);
+  }
+
   Y = date.getFullYear() + '-';
   M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
   D = date.getDate() + ' ';
